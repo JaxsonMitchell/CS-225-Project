d627ebafc0cd3f73325a9486e3d3ffd3920e4dfd
Zoe Morawek <137559861+morawekz@users.noreply.github.com>
2023-11-08 17:05:12 -0700

    Create Battler.h

---
+++ Battler.h
@@ -0,0 +1,14 @@
+
+#pragma once
+using namespace std;
+// We really do not need any includes
+class Battler {
+   protected:
+    int initative = 0;
+
+   public:
+    
+    // Using virtural to overlaod the opporator latter
+    virtual void set_initative(int num) {initative = num;}
+    int get_initative() {return initative;}
+};
