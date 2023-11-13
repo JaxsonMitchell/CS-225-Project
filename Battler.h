e41d7f6827e23c1cc6e0e11c5e4efa0ab40248a6
Zoe Morawek <137559861+morawekz@users.noreply.github.com>
2023-11-12 22:06:09 -0700

    Update Battler.h

--- Battler.h
+++ Battler.h
@@ -1,16 +1,21 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
 using namespace std;
-// We really do not need any includes
+
 class Battler {
-   protected:
-    int initative = 0;
+   private:
+    int initative;
 
    public:
     
     // Using virtural to overlaod the opporator latter
     virtual void set_initative(int num) {initative = num;}
     int get_initative() {return initative;}
-    bool operator<(const Battler& battler) {
-        return initative < battler.initative;
+    
+    bool operator<(Battler b2) {
+        return initative > b2.initative;
     }
 };
