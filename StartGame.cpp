051f90dc628c8b5dfbe3f1737bf41004a3b042c7
JaxsonMitchell <123119192+JaxsonMitchell@users.noreply.github.com>
2023-11-17 02:30:45 -0700

    Update StartGame.cpp

--- StartGame.cpp
+++ StartGame.cpp
@@ -15,30 +15,28 @@
 #include "Monster.h"
 #include "Battler.h"
 
-class Battle{
+class Battle {
     private:
-        vector<Adventurer> party;
-        vector<Monster> enemies;
-        vector<Battler> combatants;
+        vector<Adventurer>& party;
+        vector<Monster>& enemies;
+
     public:
-        Battle(
-            vector<Adventurer> party,
-            vector<Monster> enemies,
-            vector<Battler> combatants
-        ){
-            this->party = party;
-            this->enemies = enemies;
-            this->combatants = combatants;
-            
-            int randomNum;
-            
-            // Sort combatants with initiative
-            for (int i = 0; i < combatants.size(); ++i){
-                randomNum = 1 + (rand() % 20);
-                combatants[i].set_initative(randomNum);
+        Battle(vector<Adventurer>& party, vector<Monster>& enemies) : party(party), enemies(enemies) {}
+        
+        void fight(){
+            // Tests by iterating through the list.
+            cout << endl << endl;
+            printf("\e[0;31m"); // Red font
+            cout << "BATTLE BEGIN! FIGHT!" << endl;
+            printf("\e[0m \n");
+
+            for (auto& adventurer : party) {
+                cout << adventurer.getName() << endl;
             }
-            
-            sort(combatants.begin(), combatants.end());
+    
+            for (auto& monster : enemies) {
+                cout << monster.getName() << endl;
+            }
         }
 };
 
@@ -54,15 +52,16 @@ void displayStringLikeText(string text, float rateOfMessage){
 
 int main()
 {
+    // Initialization and seeding a random number generator.
     srand(time(NULL));
-
     string reply;
     int i;
+    
     cout<<"Once upon a time..."<<endl;
     sleep(2);
     cout<<"Four Adventures walk into the bar and the bartender says:"<<endl;
     sleep(3);
-    printf(" \e[0;33m \n"); //yellow font
+    printf(" \e[0;33m"); //yellow font
     displayStringLikeText("Ay-yO WhAT are YE NAMEs?", 30);
     printf("\e[0m");
     
@@ -90,17 +89,17 @@ int main()
     printf("\e[0m \n");
    
     cout<< knight.getName() << " the knight replies with a kind ";
-    printf("\e[1;34m"); //yellow font
-    cin>>reply;
+    printf("\e[0;36m"); //yellow font
+    displayStringLikeText("What..? Salt..? Of course not.", 10);
     
     
     printf("\e[0;33m"); //yellow font
-    cout<<"Why thank YE for accepting my quest to find the";
+    displayStringLikeText("Why thank YE for accepting my quest to find the", 30);
     printf(" \e[1;35m"); //purple font
-    cout<<"SALT SHAKER!"<<endl;
+    displayStringLikeText("SALT SHAKER!", 5);
     printf("\e[0;33m"); //yellow font
-    cout<<"Better be on your way then"<<endl;
-    cout<<"Bye bye!!"<<endl;
+    displayStringLikeText("Better be on your way then", 30);
+    displayStringLikeText("Bye bye!!", 100);
     printf("\e[0m \n");
     
     cout<<"and so";
@@ -110,6 +109,18 @@ int main()
     }
     cout<<" and "<<adventurers[i].getName()
         <<" all walk out of the bar and into..."<<endl;
+    
+    sleep(2);
+    cout << "A mob of Gnomes..? Trying to get into the adventurers way!!!" << endl;
+    printf("\e[1;31m"); // Red font
+    displayStringLikeText("We are GNOMEAGEDDON!", 5);
+    printf("\e[0m"); // Resets
+    cout << "The gnomes coalesce into a monstrosity never gazed upon by mankind." << endl;
+    
+    vector<Monster> gnome({Gnomeageddon()});    
+    Battle battle1(adventurers, gnome);
+    battle1.fight();
+
     
     
     return 0;
