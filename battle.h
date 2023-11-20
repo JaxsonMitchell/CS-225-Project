92d68c0bc24c9f09bca475921daa28fd9f498a3c
JaxsonMitchell <123119192+JaxsonMitchell@users.noreply.github.com>
2023-11-19 21:51:58 -0700

    Create battle.h

---
+++ battle.h
@@ -0,0 +1,28 @@
+#include "battlers.h"
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <unistd.h>
+
+void resetDefenseMultipliers(vector<Adventurer*>& vec);
+
+template <class T>
+void removeIfNoHealth(vector<T*>& vec);
+
+template <class T>
+void printOutAllMembers(vector<T*>& vec);
+
+bool neitherVectorsEmpty(vector<Adventurer*>& party, vector<Monster*>& enemies);
+
+class Battle {
+private:
+    vector<Adventurer*>& party;
+    vector<Monster*>& enemies;
+public:
+    Battle(vector<Adventurer*>& party, vector<Monster*>& enemies);
+    void fight();
+};
+
+void displayStringLikeText(string text, float rateOfMessage);
